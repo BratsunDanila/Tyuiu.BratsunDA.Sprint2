fb9448926823610aada2bb83f6163163e0fb746e
BratsunDanila <Tyuiu.BratsunDA@outlook.com>
2023-10-12 19:17:25 +0500

    Add project files.

---
+++ Tyuiu.BratsunDA.Sprint2.Task0.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BratsunDA.Sprint2.Task0.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BratsunDA.Sprint2.Task0.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95d1273d-54dd-4c36-b520-23baccef9cfe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BratsunDA.Sprint2.Task0.V28/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BratsunDA.Sprint2.Task0.V28.Lib;
+
+namespace Tyuiu.BratsunDA.Sprint2.Task0.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 111;
+            int y = 735;
+
+            Console.Title = "Спринт #2| Выполнил: Брацун Д.А. | ПКТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил Брацун Д.А. | ПКТБ-23-1                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=)         *");
+            Console.WriteLine("* и арифметических выражений, которая вернет массив.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
